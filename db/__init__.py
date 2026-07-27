ccfbba8dc7811d1e328bb5227118c5a9776bbb14
harshagm665-netizen <harshagm665@gmail.com>
2026-07-27 23:57:06 +0530

    Refactor: Delete duplicated root-level legacy code to solidify functions/ as single source of truth

--- db/__init__.py
+++
@@ -1,4 +0,0 @@
-"""db/__init__.py"""
-from .sqlite_client import db, JobDB
-
-__all__ = ["db", "JobDB"]
--- llm/__init__.py
+++
@@ -1,4 +0,0 @@
-"""llm/__init__.py"""
-from .client import llm, LLMClient, LLMError
-
-__all__ = ["llm", "LLMClient", "LLMError"]
